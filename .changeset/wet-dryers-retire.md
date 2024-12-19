88d5d02972b918ce90874de3d902a484b777234f
ocavue <ocavue@gmail.com>
2024-12-19 13:52:34 +1100

    feat: increase heading level if project comment includes heading

---
+++ .changeset/wet-dryers-retire.md
@@ -0,0 +1,5 @@
+---
+'typedoc-plugin-md': minor
+---
+
+Automatically adjust heading level based on the presence of heading in the `@packageDocumentation` and `@module` comments.
--- packages/typedoc-plugin-md/src/partials/container.ts
+++ packages/typedoc-plugin-md/src/partials/container.ts
@@ -1,5 +1,6 @@
 import type * as mdast from 'mdast'
 import type { ContainerReflection } from 'typedoc'
+import { visit } from 'unist-util-visit'
 
 import type { MarkdownThemeContext } from '../theme/markdown-theme-context'
 import * as m from '../utils/mdast'
@@ -11,22 +12,35 @@ export function getContainer(
   ctx: MarkdownThemeContext,
   project: ContainerReflection,
 ): mdast.Root {
+  const projectComments = getComment(ctx, project.comment, { headingLevel: 2 })
+
+  let hasHeading = false
+  visit(m.root(projectComments), 'heading', (node) => {
+    if (node.type === 'heading') {
+      hasHeading = true
+    }
+  })
+
   return m.root([
     m.heading(1, [m.text(project.name)]),
-    ...getComment(ctx, project.comment, { headingLevel: 2 }),
-    ...getProjectChildren(ctx, project),
+    ...projectComments,
+    ...(hasHeading ? [m.heading(2, [m.text('API Reference')])] : []),
+    ...getProjectChildren(ctx, project, { headingLevel: hasHeading ? 3 : 2 }),
   ])
 }
 
 function getProjectChildren(
   ctx: MarkdownThemeContext,
   project: ContainerReflection,
+  options: {
+    headingLevel: m.HeadingLevel
+  },
 ): mdast.RootContent[] {
   const children = project.children || []
 
   return [
     ...children.flatMap((child) =>
-      getDeclaration(ctx, child, { headingLevel: 2 }),
+      getDeclaration(ctx, child, { headingLevel: options.headingLevel }),
     ),
   ]
 }
