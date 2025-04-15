2de15f8d75000b5100341aa06b727d35cd79de71
ocavue <ocavue@gmail.com>
2025-04-15 15:41:30 +1000

    copy member.signature.ts

--- packages/typedoc-plugin-md-v2/src/markdown-theme-context.ts
+++ packages/typedoc-plugin-md-v2/src/markdown-theme-context.ts
@@ -1,14 +1,18 @@
 import { MarkdownThemeContext } from 'typedoc-plugin-markdown'
 
+import { partials } from './theme/context'
+
 export class MyMarkdownThemeContext extends MarkdownThemeContext {
   constructor(...args: ConstructorParameters<typeof MarkdownThemeContext>) {
     super(...args)
 
-    const partials = { ...this.partials }
+    const originalPartials = { ...this.partials }
 
     this.partials.signatureTitle = (...args) => {
-      const result = partials.signatureTitle(...args)
+      const result = originalPartials.signatureTitle(...args)
       return `<!-- DEBUG_signatureTitle -->` + '\n\n' + result
     }
+
+    this.partials.signature = (...args) => partials.signature.apply(this, args)
   }
 }
